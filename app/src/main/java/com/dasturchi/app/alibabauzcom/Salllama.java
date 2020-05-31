53202dc3ed282872c3701e3f1db3cd94f60867ca
Brain1999 <bunyodbek199611133961133@gmail.com>
2020-05-31 11:06:07 +0500

    Second Commit

---
+++ app/src/main/java/com/dasturchi/app/alibabauzcom/Salllama.java
@@ -0,0 +1,16 @@
+package com.dasturchi.app.alibabauzcom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mygains.app.alibabauzcom.R;
+
+public class Salllama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salllama);
+    }
+}
